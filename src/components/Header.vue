f3fabe4aa930a2e7944db40a217b829b96cec481
Davron <shermatovdavron6@gmail.com>
2024-04-01 20:36:31 +0500

    vue

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<script setup>
+
+import Button from "@/components/icons/Button.vue";
+</script>
+
+<template>
+  <div class="container">
+    <nav class="nav">
+      <div class="nav_logo">
+        <a href="#">
+          <img src="@/assets/img/logo.png" alt="Logo">
+        </a>
+      </div>
+      <ul class="nav_list">
+        <li><a href="#">ALL PRODUCTS</a></li>
+        <li><a href="#">ABOUT SEEDRA</a></li>
+        <li><a href="#">OUR BLOG</a></li>
+        <li><a href="#">CONTACT</a></li>
+      </ul>
+      <Search/>
+      <Button className="btn-icon-card"/>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 1440px;
+  height: 72px;
+  box-shadow: 12px 0 #CFCFCF40;
+  .nav_list{
+    display: flex;
+    list-style: none;
+    a{
+      text-decoration: none;
+      color:#70737C ;
+      font-weight: 400px;
+    }
+  }
+}
+</style>
---
+++ src/components/icons/Input.vue
@@ -0,0 +1,43 @@
+<script setup>
+defineProps(['type','placeholder','label','place_color'])
+</script>
+
+<template>
+  <div class="input">
+    <label >{{label}}</label>
+    <input :type="type" :placeholder="placeholder">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.input{
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  line-height:20px;
+  label{
+    color: #70737C;
+    font-weight: 400;
+    font-size: 14px;
+  }
+  input{
+    border-radius: 4px;
+    width: 350px;
+    height: 56px;
+    border: 1px solid #EFEFEF;
+    &:hover{
+      border: 1px solid #EFEFEF ;
+      box-shadow:0 1px 6px 0 #00000052 ;
+    }
+    ::placeholder{
+      color: #70737C;
+    }
+    &:focus,&:active{
+      border: 1px solid #EFEFEF ;
+      box-shadow:0 1px 6px 0 #00000052 ;
+    }
+
+  }
+}
+</style>
